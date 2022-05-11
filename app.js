45acc7da348d4d5191bb19e107758f9c392b1fa6
Wanhsuan98 <wanhsuan98@gmail.com>
2022-05-11 14:57:36 +0800

    2-1 建立 Server 並監聽變化（ 內建模組 http）

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //block 1 : 內建模組
 const path = require('path')
+const http = require('http')
+
 
 //block 2 : 第三方模組
-const cowsay = require('cowsay');
 
 //block 3 : 自建模組
 
@@ -10,11 +11,26 @@ const cowsay = require('cowsay');
 
 //////////////////////////////////////////////////
 //block 4 : 開始執行
-console.log(cowsay.say({
-    text: "I'm John Cecow",
-    e: "TT",
-    T: "U "
-}));
+
+const server = http.createServer((req, res) => {
+    //console.log('第一個參數是瀏覽器對 web server 的 request', req);
+    //console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
+    //console.log('req url:', req.url);
+    if (req.url === '/') {
+        //console.log('login page')
+        return res.end('this is home page');
+    }
+    if (req.url === '/login') {
+        //console.log('login page')
+        return res.end('this is login page');
+    }
+
+    res.end('page not found');
+});
+
+server.listen(3000, () => {
+    console.log('running server on port 3000');
+});
 //console.log(path.join(__dirname, 'index.js'));
 
 
