c2f53084ebbd906c99957d1bd347df2b0d51603d
Wanhsuan98 <wanhsuan98@gmail.com>
2022-05-11 11:31:56 +0800

    1-2-2 使用path module

--- hello.js
+++ hello.js
@@ -1,4 +1,6 @@
 // hello.js
+const path = require('path')
+
 const title = 'I am hello module'
 
 const sayHello = () => {
@@ -16,8 +18,17 @@ const sayGoodnight = () => {
 //     title: 'I am hello module'
 // };
 
+console.log('dirname', __dirname);
+
+console.log('filename', __filename);
+
+console.log(path.join(__dirname, 'index.js'));
+
+
 module.exports = {
     sayHello,
     sayGoodnight,
     title: 'I am hello module'
 };
+
+console.log('module', module);
