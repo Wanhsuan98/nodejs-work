0cb560d5779553f34a6d9908567bda178b19fd92
Wanhsuan98 <wanhsuan98@gmail.com>
2022-05-11 13:35:51 +0800

    1-5 第一次使用第三方套件就上手（第三方套件 cowsay）

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const hello = require('./hello')
-hello.sayHello();
-console.log(hello.title)
+//block 1 : 內建模組
+const path = require('path')
+
+//block 2 : 第三方模組
+const cowsay = require('cowsay');
+
+//block 3 : 自建模組
+
+//const hello = require('./hello')
+
+
+//block 4 : 開始執行
+console.log(cowsay.say({
+    text: "I'm John Cecow",
+    e: "oO",
+    T: "U "
+}));
+//console.log(path.join(__dirname, 'index.js'));
+
+
+// hello.sayHello();
+// console.log(hello.title)
+//console.log('module', module);
